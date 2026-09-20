e8b020006048ea441ba64aee086a0ca978faa120
syedomaan <syedomaan2007@gmail.com>
2026-09-20 14:38:37 +0530

    Create Compund_Interest.c

---
+++ C-Programming/Compund_Interest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int p, n, t;
+    double r, TA, CI;
+    for(;;)
+{
+    printf("Principal amount:\n");
+    scanf("%d", &p);
+
+    printf("Rate:\n");
+    scanf("%lf", &r);
+
+    printf("Compounded per year\n");
+    scanf("%d",&n);
+
+    printf("Time:\n");
+    scanf("%d", &t);
+
+ TA = p * pow(1 + (r / 100) /n,n*t);
+
+    CI = TA - p;
+
+printf("Totalinterest=%.2lf\n",TA); printf("Compoundinterest=%.2lf",C);
+}
+    return 0;
+}
