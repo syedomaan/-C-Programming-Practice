5202e7a1a66bbb2e3431bd63808c0473c30bd06f
syedomaan <syedomaan2007@gmail.com>
2026-09-20 13:42:16 +0530

    Create Simple_Interest.c

---
+++ C-Programming/Simple_Interest.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int p, r, t, si;
+    for(;;)
+    {
+      printf("Principal amount:\n");
+      scanf("%d", &p);
+
+        printf("Time:\n");
+        scanf("%d", &t);
+
+        printf("Rate:\n");
+        scanf("%d", &r);
+
+        si = p * r * t / 100;
+
+ printf("Simple Interest=%d\n",si);
+    }
+    return 0;
+}
